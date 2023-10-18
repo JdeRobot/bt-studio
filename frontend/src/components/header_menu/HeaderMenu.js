d47d020119855acd985dedfd67d343d341b69752
Óscar Martínez <oscar.personal@tutanota.com>
2023-10-18 13:59:06 +0200

    Updating gitignore and error in frontend

--- frontend/src/components/header_menu/HeaderMenu.js
+++ frontend/src/components/header_menu/HeaderMenu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import './common.css'
 
 const HeaderMenu = () => {
   return (
