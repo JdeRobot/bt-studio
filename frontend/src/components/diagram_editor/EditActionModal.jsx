f9ed6a2d1e52d836b36cc140251bf652311a7904
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-05-25 17:14:38 +0200

    Updating colors correctly

--- frontend/src/components/diagram_editor/EditActionModal.jsx
+++ frontend/src/components/diagram_editor/EditActionModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { BasicNodeWidget } from './nodes/basic_node/BasicNodeWidget.tsx';
 
-import { Saturation, Hue, ColorPicker, useColor } from "react-color-palette";
+import { Saturation, Hue, ColorPicker, useColor, TColor } from "react-color-palette";
 import "react-color-palette/css";
 
 import './EditActionModal.css';
@@ -19,8 +18,44 @@ const initialEditActionModalData = {
   newOutputName: '',
 };
 
+function rgb2hsv (r, g, b) {
+    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    rabs = r / 255;
+    gabs = g / 255;
+    babs = b / 255;
+    v = Math.max(rabs, gabs, babs);
+    diff = v - Math.min(rabs, gabs, babs);
+    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    percentRoundFn = num => Math.round(num * 100) / 100;
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(rabs);
+        gg = diffc(gabs);
+        bb = diffc(babs);
+
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    return {
+        h: Math.round(h * 360),
+        s: percentRoundFn(s * 100),
+        v: percentRoundFn(v * 100)
+    };
+}
+
 const EditActionModal = ({ isOpen, onClose, currentActionNode, setColorActionNode, addInputPort, addOutputPort, deleteInputPort, deleteOutputPort}) => {
-  // const [color, setColor] = useColor(currentActionNode ? currentActionNode.getColor().replaceAll(",", " ") : "rgb(128 0 128)");
   const focusInputRef = useRef(null);
   const [color, setColor] = useColor("rgb(128 0 128)");
   const [inputName, setInputName] = React.useState(false);
@@ -45,6 +80,14 @@ const EditActionModal = ({ isOpen, onClose, currentActionNode, setColorActionNod
     setOutputName(false)
     setFormState(initialEditActionModalData);
     document.getElementById('node-editor-modal').focus();
+    var colorRGB = currentActionNode.getColor().split('(')[1].split(')')[0].split(',')
+    color.rgb['r'] = colorRGB[0]
+    color.rgb['g'] = colorRGB[1]
+    color.rgb['b'] = colorRGB[2]
+    var hsvColor = rgb2hsv(colorRGB[0], colorRGB[1], colorRGB[2]);
+    color.hsv['h'] = hsvColor['h']
+    color.hsv['s'] = hsvColor['s']
+    setColor(color)
   }, [isOpen]);
 
   useEffect(() => {
