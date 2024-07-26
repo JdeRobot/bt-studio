fe99b130235a6e44cb80cd19eee70a0aea47f8f7
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-07-26 12:12:35 +0200

    Better checkboxes

--- frontend/src/components/settings_popup/options/Checkbox.jsx
+++ frontend/src/components/settings_popup/options/Checkbox.jsx
@@ -1,22 +1,13 @@
 import React, { useMemo, useState, useEffect } from 'react';
 
+import './Checkbox.css';
+
 const Checkbox = ({value, setValue}) => {
-  const [open, setOpen] = useState(false);
 
   return (
-    // <div className="settings-checkbox">
-    //   <button onClick={(e) => { e.preventDefault(); setOpen(!open)}}>{value}</button>
-    //   {open &&
-    //     <ul className="settings-dropdown-list">
-    //       {possibleValues.map((name, index) => (
-    //         <li key={name} className="settings-dropdown-item">
-    //           <button onClick={(e) => changeValue(e,name)}>{name}</button>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   }
-    // </div>
-    <input type="checkbox" className='setting-setting-checkbox' checked={value} onChange={() => setValue(!value)}/>
+    <div className="setting-checkbox-wrapper">
+      <input type="checkbox" checked={value} onChange={() => setValue(!value)}/>
+    </div>
   );
 };
 
