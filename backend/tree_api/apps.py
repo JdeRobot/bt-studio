e1393ea11ed41ceba4ff5b0f77b4fa0ac629f09f
Óscar Martínez <oscar.personal@tutanota.com>
2024-07-31 13:59:26 +0200

    Std and cleaning

--- backend/tree_api/apps.py
+++ backend/tree_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TreeApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tree_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tree_api"
--- backend/tree_api/serializers.py
+++ backend/tree_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
 
+
 class FileContentSerializer(serializers.Serializer):
     content = serializers.CharField()
