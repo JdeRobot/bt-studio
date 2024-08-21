67d6cb7bdfeef534a640bf9dfd1ceb11a3c112e7
Óscar Martínez <oscar.personal@tutanota.com>
2024-08-21 22:25:15 +0200

    Restored basic menu and ts config

--- frontend/src/components/diagram_editor/NodeMenu.tsx
+++ frontend/src/components/diagram_editor/NodeMenu.tsx
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import "./NodeHeader.css";
 import axios from "axios";
+
+import { ReactComponent as DeleteIcon } from "./img/del_node.svg";
+import { ReactComponent as EditActionIcon } from "./img/edit_action.svg";
+import { ReactComponent as HelpIcon } from "./img/help.svg";
+import { ReactComponent as DownloadIcon } from "./img/download.svg";
+import { ReactComponent as RunIcon } from "./img/run.svg";
+import { ReactComponent as StopIcon } from "./img/stop.svg";
+import { ReactComponent as ZoomToFitIcon } from "./img/zoom_to_fit.svg";
+import { ReactComponent as ResetIcon } from "./img/reset.svg";
 
 var NODE_MENU_ITEMS: Record<string, string[]> = {
   Sequences: ["Sequence", "ReactiveSequence", "SequenceWithMemory"],
@@ -42,9 +51,15 @@ const fetchActionList = async (project_name: string) => {
 const NodeMenu = ({
   projectName,
   onAddNode,
+  onDeleteNode,
+  onZoomToFit,
+  onEditAction,
 }: {
   projectName: string;
   onAddNode: Function;
+  onDeleteNode: MouseEventHandler;
+  onZoomToFit: MouseEventHandler;
+  onEditAction: MouseEventHandler;
 }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [menuLabel, setMenuLabel] = useState<string>("");
@@ -67,6 +82,15 @@ const NodeMenu = ({
     console.log("Selected: " + nodeName);
     onAddNode(nodeName);
     handleClose();
+  };
+
+  const openInNewTab = (url: URL) => {
+    const newWindow = window.open(url, "_blank");
+    if (newWindow) {
+      newWindow.focus();
+    } else {
+      console.error("Failed to open new tab/window.");
+    }
   };
 
   return (
@@ -92,6 +116,47 @@ const NodeMenu = ({
           </MenuItem>
         ))}
       </Menu>
+
+      <div className="action-buttons">
+        <button
+          id="node-action-delete-button"
+          className="node-action-button"
+          onClick={onDeleteNode}
+          title="Delete"
+        >
+          <DeleteIcon className="icon action-icon" fill={"var(--icon)"} />
+        </button>
+        <button
+          id="node-action-edit-button"
+          className="node-action-button"
+          onClick={onEditAction}
+          title="Edit"
+        >
+          <EditActionIcon className="icon action-icon" stroke={"var(--icon)"} />
+        </button>
+        <button
+          id="node-action-zoom-button"
+          className="node-action-button"
+          onClick={onZoomToFit}
+          title="Zoom To Fit"
+        >
+          <ZoomToFitIcon className="icon action-icon" fill={"var(--icon)"} />
+        </button>
+        <button
+          id="node-action-help-button"
+          className="node-action-button"
+          onClick={() => {
+            openInNewTab(
+              new URL(
+                "https://github.com/JdeRobot/bt-studio/tree/unibotics-devel/documentation"
+              )
+            );
+          }}
+          title="Help"
+        >
+          <HelpIcon className="icon action-icon" fill={"var(--icon)"} />
+        </button>
+      </div>
     </div>
   );
 };
