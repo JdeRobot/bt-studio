5342727c2062827c3c7372647dca704613ebc6c9
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-08-26 17:47:31 +0200

    Adding rename button to top view

--- frontend/src/components/file_browser/FileBrowser.js
+++ frontend/src/components/file_browser/FileBrowser.js
@@ -12,6 +12,7 @@ import { ReactComponent as AddIcon } from "./img/add.svg";
 import { ReactComponent as AddFolderIcon } from "./img/add_folder.svg";
 import { ReactComponent as DeleteIcon } from "./img/delete.svg";
 import { ReactComponent as RefreshIcon } from "./img/refresh.svg";
+import { ReactComponent as RenameIcon } from "./img/rename.svg";
 
 function getParentDir(file) {
   // Check if is a directory and if not get the parent directory of the file
@@ -63,7 +64,7 @@ const FileBrowser = ({
     if (currentProjectname !== "") {
       try {
         const response = await axios.get(
-          `/tree_api/get_file_list?project_name=${currentProjectname}`,
+          `/tree_api/get_file_list?project_name=${currentProjectname}`
         );
         const files = JSON.parse(response.data.file_list);
         setFileList(files);
@@ -98,13 +99,13 @@ const FileBrowser = ({
         switch (data.fileType) {
           case "actions":
             response = await axios.get(
-              `/tree_api/create_action?project_name=${currentProjectname}&filename=${data.fileName}.py&template=${data.templateType}`,
+              `/tree_api/create_action?project_name=${currentProjectname}&filename=${data.fileName}.py&template=${data.templateType}`
             );
             break;
           default:
             // TODO:  create plain file
             response = await axios.get(
-              `/tree_api/create_file?project_name=${currentProjectname}&location=${location}&folder_name=${data.fileName}`,
+              `/tree_api/create_file?project_name=${currentProjectname}&location=${location}&folder_name=${data.fileName}`
             );
             break;
         }
@@ -141,7 +142,7 @@ const FileBrowser = ({
     if (deleteEntry) {
       try {
         const response = await axios.get(
-          `/tree_api/delete_file?project_name=${currentProjectname}&path=${deleteEntry}`,
+          `/tree_api/delete_file?project_name=${currentProjectname}&path=${deleteEntry}`
         );
         if (response.data.success) {
           setProjectChanges(true);
@@ -186,7 +187,7 @@ const FileBrowser = ({
     if (folder_name !== "") {
       try {
         const response = await axios.get(
-          `/tree_api/create_folder?project_name=${currentProjectname}&location=${location}&folder_name=${folder_name}`,
+          `/tree_api/create_folder?project_name=${currentProjectname}&location=${location}&folder_name=${folder_name}`
         );
         if (response.data.success) {
           setProjectChanges(true);
@@ -220,7 +221,7 @@ const FileBrowser = ({
     if (renameEntry) {
       try {
         const response = await axios.get(
-          `/tree_api/rename_file?project_name=${currentProjectname}&path=${renameEntry.path}&rename_to=${new_path}`,
+          `/tree_api/rename_file?project_name=${currentProjectname}&path=${renameEntry.path}&rename_to=${new_path}`
         );
         if (response.data.success) {
           setProjectChanges(true);
@@ -243,8 +244,9 @@ const FileBrowser = ({
 
   const handleRenameCurrentFile = () => {
     //TODO: need to obtain all file data to do this
+    return;
     if (currentFilename) {
-      handleDeleteModal(currentFilename);
+      handleRename(currentFilename);
     } else {
       alert("No file is currently selected.");
     }
@@ -324,9 +326,14 @@ const FileBrowser = ({
           </button>
           <div style={{ marginLeft: "auto" }} />
           {currentFilename !== "" && (
-            <button onClick={handleDeleteCurrentFile} title="Delete file">
-              <DeleteIcon className="icon" fill={"var(--icon)"} />
-            </button>
+            <>
+              <button onClick={handleRenameCurrentFile} title="Rename file">
+                <RenameIcon className="icon" stroke={"var(--icon)"} />
+              </button>
+              <button onClick={handleDeleteCurrentFile} title="Delete file">
+                <DeleteIcon className="icon" fill={"var(--icon)"} />
+              </button>
+            </>
           )}
         </div>
         <FileExplorer
