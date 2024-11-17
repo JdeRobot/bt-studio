b4c1153c56a64702e1f2a215dad7c219b3cadad6
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-11-17 11:53:55 +0100

    Change db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'academy_db' ,
+        'USER': 'user-dev',
+        'PASSWORD': 'bt-studio-dev',
+        'HOST': 'my-postgres',
+        'PORT': '5432',
     }
 }
 
