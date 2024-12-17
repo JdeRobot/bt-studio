27ab8cc4035f1157fa140b17de5ca7ab840b9765
Javier Izquierdo Hernandez <javizqh@gmail.com>
2024-12-17 09:34:18 +0100

    Add icon and buttons

--- frontend/src/components/status_bar/StatusBar.tsx
+++ frontend/src/components/status_bar/StatusBar.tsx
@@ -3,6 +3,8 @@ import { OptionsContext } from "../options/Options";
 
 import "./StatusBar.css";
 
+import { ReactComponent as TerminalIcon } from "./img/terminal.svg";
+
 const StatusBar = ({
 }: {
 }) => {
@@ -11,6 +13,28 @@ const StatusBar = ({
 
   return (
     <div className="status-bar-container">
+      <button
+        className="status-bar-button"
+        onClick={() => console.log("a")}
+        title="Toggle console"
+      >
+        <TerminalIcon className="status-bar-icon" stroke={"var(--icon)"} />
+      </button>
+      <button
+        className="status-bar-button"
+        onClick={() => console.log("a")}
+        title="Toggle console"
+      >
+        <TerminalIcon className="status-bar-icon" stroke={"var(--icon)"} />
+      </button>
+      <button
+        className="status-bar-button"
+        onClick={() => console.log("a")}
+        title="Toggle Gazebo"
+        style={{marginLeft:"auto"}}
+      >
+        <TerminalIcon className="status-bar-icon" stroke={"var(--icon)"} />
+      </button>
     </div>
   );
 };
