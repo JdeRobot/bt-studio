2d854f348848a22735030efcf07c7188dd00492d
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-08-25 19:10:24 +0200

    Linter

--- backend/filesystem/warehouse_demo/code/actions/CalculateSpeed.py
+++ backend/filesystem/warehouse_demo/code/actions/CalculateSpeed.py
@@ -3,10 +3,12 @@ import geometry_msgs
 import tree_tools
 from math import atan2, pi
 
+
 def getSign(value):
     if value < 0:
         return -1
     return 1
+
 
 def getTurnDirection(robot_yaw, goal_yaw):
     if goal_yaw > robot_yaw:
@@ -22,11 +24,11 @@ def getTurnDirection(robot_yaw, goal_yaw):
 
     return direction
 
+
 class CalculateSpeed(py_trees.behaviour.Behaviour):
 
-    def __init__(self, name, ports = None):
-
-        """ Constructor, executed when the class is instantiated """
+    def __init__(self, name, ports=None):
+        """Constructor, executed when the class is instantiated"""
 
         # Configure the name of the behavioure
         super().__init__(name)
@@ -36,38 +38,35 @@ class CalculateSpeed(py_trees.behaviour.Behaviour):
         self.ports = ports
 
     def setup(self, **kwargs: int) -> None:
-
-        """ Executed when the setup function is called upon the tree """
+        """Executed when the setup function is called upon the tree"""
 
         # Get the node passed from the tree (needed for interaction with ROS)
         try:
-            self.node = kwargs['node']
+            self.node = kwargs["node"]
         except KeyError as e:
             error_message = "Couldn't find the tree node"
             raise KeyError(error_message) from e
 
     def initialise(self) -> None:
-
-        """ Executed when coming from an idle state """
+        """Executed when coming from an idle state"""
 
         # Debugging
         self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
     def update(self) -> py_trees.common.Status:
-
-        """ Executed when the action is ticked. Do not block! """
+        """Executed when the action is ticked. Do not block!"""
 
         # Publish the speed msg
         target_x = float(tree_tools.get_port_content(self.ports["target_x"]))
         target_y = float(tree_tools.get_port_content(self.ports["target_y"]))
-   
+
         x = float(tree_tools.get_port_content(self.ports["x"]))
         y = float(tree_tools.get_port_content(self.ports["y"]))
         yaw = float(tree_tools.get_port_content(self.ports["yaw"]))
 
         x_diff = target_x - x
         y_diff = target_y - y
-        target_yaw =  atan2(y_diff, x_diff)
+        target_yaw = atan2(y_diff, x_diff)
 
         if abs(target_yaw - yaw) > 0.01:  # No aligned
             ang_speed = getTurnDirection(yaw, target_yaw) * 0.03
@@ -85,8 +84,10 @@ class CalculateSpeed(py_trees.behaviour.Behaviour):
         return py_trees.common.Status.SUCCESS
 
     def terminate(self, new_status: py_trees.common.Status) -> None:
-
-        """ Called whenever the behaviour switches to a non-running state """
+        """Called whenever the behaviour switches to a non-running state"""
 
         # Debugging
-        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug(
+            "%s.terminate()[%s->%s]"
+            % (self.__class__.__name__, self.status, new_status)
+        )
--- backend/filesystem/warehouse_demo/code/actions/SetSpeed.py
+++ backend/filesystem/warehouse_demo/code/actions/SetSpeed.py
@@ -2,11 +2,11 @@ import py_trees
 import geometry_msgs
 import tree_tools
 
+
 class SetSpeed(py_trees.behaviour.Behaviour):
 
-    def __init__(self, name, ports = None):
-
-        """ Constructor, executed when the class is instantiated """
+    def __init__(self, name, ports=None):
+        """Constructor, executed when the class is instantiated"""
 
         # Configure the name of the behavioure
         super().__init__(name)
@@ -16,16 +16,14 @@ class SetSpeed(py_trees.behaviour.Behaviour):
         self.ports = ports
 
     def setup(self, **kwargs: int) -> None:
-
-        """ Executed when the setup function is called upon the tree """
+        """Executed when the setup function is called upon the tree"""
 
         # Get the node passed from the tree (needed for interaction with ROS)
         try:
-            self.node = kwargs['node']
+            self.node = kwargs["node"]
         except KeyError as e:
             error_message = "Couldn't find the tree node"
             raise KeyError(error_message) from e
-
 
         topic = tree_tools.get_port_content(self.ports["topic"])
 
@@ -34,15 +32,13 @@ class SetSpeed(py_trees.behaviour.Behaviour):
         )
 
     def initialise(self) -> None:
-
-        """ Executed when coming from an idle state """
+        """Executed when coming from an idle state"""
 
         # Debugging
         self.logger.debug("%s.initialise()" % (self.__class__.__name__))
 
     def update(self) -> py_trees.common.Status:
-
-        """ Executed when the action is ticked. Do not block! """
+        """Executed when the action is ticked. Do not block!"""
 
         # Publish the speed msg
         msg = geometry_msgs.msg.Twist()
@@ -54,8 +50,10 @@ class SetSpeed(py_trees.behaviour.Behaviour):
         return py_trees.common.Status.SUCCESS
 
     def terminate(self, new_status: py_trees.common.Status) -> None:
-
-        """ Called whenever the behaviour switches to a non-running state """
+        """Called whenever the behaviour switches to a non-running state"""
 
         # Debugging
-        self.logger.debug("%s.terminate()[%s->%s]" % (self.__class__.__name__, self.status, new_status))
+        self.logger.debug(
+            "%s.terminate()[%s->%s]"
+            % (self.__class__.__name__, self.status, new_status)
+        )
